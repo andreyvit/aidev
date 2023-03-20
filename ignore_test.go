d2c9b53744a819189025407b9e84dd84fd7a9d49
Andrey Tarantsov <andrey@tarantsov.com>
2023-03-20 12:06:20 +0400

    Add test suite (via AI)

---
+++ ignore_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMatch(t *testing.T) {
+	tests := []struct {
+		name     string
+		patterns []string
+		path     string
+		dir      string
+		isDir    bool
+		expected int
+	}{
+		{
+			name:     "simple include pattern",
+			patterns: []string{"*.go"},
+			path:     "main.go",
+			dir:      ".",
+			isDir:    false,
+			expected: 4,
+		},
+		{
+			name:     "path include pattern",
+			patterns: []string{"frontend/*.js"},
+			path:     "frontend/app.js",
+			dir:      "frontend",
+			isDir:    false,
+			expected: 12,
+		},
+		// Add more test cases
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := match(test.path, test.dir, test.isDir, test.patterns)
+			if result != test.expected {
+				t.Errorf("Expected %d, got %d", test.expected, result)
+			}
+		})
+	}
+}
+
+func TestShouldIgnore(t *testing.T) {
+	tests := []struct {
+		name     string
+		ignorer  *Ignorer
+		path     string
+		isDir    bool
+		expected bool
+	}{
+		{
+			name: "simple include pattern",
+			ignorer: newIgnorer(&TreeConfig{
+				Includes: []string{"*.go"},
+			}, ""),
+			path:     "main.go",
+			isDir:    false,
+			expected: false,
+		},
+		{
+			name: "path include pattern",
+			ignorer: newIgnorer(&TreeConfig{
+				Includes: []string{"frontend/*.js"},
+			}, ""),
+			path:     "frontend/app.js",
+			isDir:    false,
+			expected: false,
+		},
+		// Add more test cases
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := test.ignorer.ShouldIgnore(test.path, test.isDir)
+			if result != test.expected {
+				t.Errorf("Expected %t, got %t", test.expected, result)
+			}
+		})
+	}
+}
